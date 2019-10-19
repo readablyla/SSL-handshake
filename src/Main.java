3cd4e28ab7ba4035d5c4fcd62a05fbb95583bd2b
leala1 <leala133@outlook.com>
2019-10-19 18:16:42 +1100

    RSA working

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.math.BigInteger;
 import java.net.*; // for Socket programming
 import java.security.MessageDigest; // for SHA256
+import java.security.NoSuchAlgorithmException; // SHA256
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         //int p = 178011905478542266528237562450159990145232156369120674273274450314442865788737020770612695252123463079567156784778466449970650770920727857050009668388144034129745221171818506047231150039301079959358067395348717066319802262019714966524135060945913707594956514672855690606794135837542707371727429551343320695239;
         //int g = 174068207532402095185811980123523436538604490794561350978495831040599953488455823147851597408940950725307797094915759492368300574252438761037084473467180148876118103083043754985190983472601550494691329488083395492313850000361646482644608492304078721818959999056496097769368017749273708962006689187956744210730
 
@@ -14,18 +18,35 @@ public class Main {
         BigInteger e = new BigInteger("8395559");
         BigInteger n = new BigInteger("655375");
         BigInteger result = fastModExp(b,e,n);
-        System.out.println("Result is " + result);
+        //System.out.println("Result is " + result);
 
         //RSA
         Random rsa_random = new Random();
         //double check 2048 and 100 are the right numbers to use here
-        BigInteger p = new BigInteger(2048, 100, rsa_random);
-        BigInteger q = new BigInteger(2048, 100, rsa_random);
+        BigInteger p = new BigInteger(2048, 100, rsa_random); //must be a prime?
+        BigInteger q = new BigInteger(2048, 100, rsa_random); //must be a prime?
+        BigInteger m = (p.subtract(BigInteger.ONE)).multiply((q.subtract(BigInteger.ONE)));
         BigInteger rsa_e = new BigInteger("65537");
         BigInteger rsa_n = p.multiply(q);
-        BigInteger[] rsa_servers_public_key = new BigInteger[2];
-        rsa_servers_public_key[0] = rsa_n;
-        rsa_servers_public_key[1] = rsa_e;
+        BigInteger d = rsa_e.modInverse(m);
+
+        // I thought that a public key was the (n, e) pair, but it says that the public key is
+        //the fixed e=65537. This is the message to be sent?
+
+        // find SHA256 of rsa_e - check it is ok to convert rsa_e the 'public key'? into a String for this
+        MessageDigest mdigest = MessageDigest.getInstance("SHA-256"); //hashing this directly gives 102/e3b pair
+        mdigest.update(rsa_e.toString().getBytes(StandardCharsets.UTF_8)); // getBytes works if rsa_e is a string. convert e to byte array and update the mdigest with this value. next call will operate on the update, and return correct value
+        String output = String.format("%064x", new BigInteger(1, mdigest.digest()));//digest the original mdigest, format result as a string
+        BigInteger sha_bigInt = new BigInteger(output, 16);
+        System.out.println("HashedString: " + output+"\nHashedString as dec: "+sha_bigInt);//correctly converts hex to dec
+
+        // RSA signature generation
+        BigInteger s = fastModExp(sha_bigInt, d, rsa_n);
+        System.out.println("b: " + sha_bigInt + "\ne: " + d + "\nn: " + rsa_n);
+        System.out.println("Result of fastModExp = s: " + s);
+        // RSA signature verification
+        BigInteger check_h_m = fastModExp(s, rsa_e, rsa_n);
+        System.out.println("Compare sha_bigInt: " + sha_bigInt + "\nvs check_h_m: " + check_h_m);
 
         /*//Diffie-Hellman
         int p = 23;
